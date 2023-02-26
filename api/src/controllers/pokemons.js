4e25b48b5b14284cfc85ef7020844d98383897ca
Jonathan Kim <jonnykimbk@hotmail.com>
2023-02-26 20:23:05 -0300

    menos pokemons

--- api/src/controllers/pokemons.js
+++ api/src/controllers/pokemons.js
@@ -11,7 +11,7 @@ let pokemons = [];
 
 const promisePokemons = [];
 
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= 60; i++) {
   promisePokemons.push(`https://pokeapi.co/api/v2/pokemon/${i}`);
 }
 
